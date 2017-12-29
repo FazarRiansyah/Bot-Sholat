d4a86490c15325b914d815008b572940a3f01473
FazarRiansyah <34387399+FazarRiansyah@users.noreply.github.com>
2017-12-29 23:58:50 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,13 +1,8 @@
 <?php
-/*
-copyright @ medantechno.com
-Modified @ Farzain - zFz
-2017
-*/
 require_once('./line_class.php');
 require_once('./unirest-php-master/src/Unirest.php');
-$channelAccessToken = 'pXaYg3+bY2fU1yYFMZ1zSymgm74qpiqeMMsefjyZPQ3AhpPrCd8yc1FWXsX5dp95tpDmU9ludYBWThoiPiDsOmnTATSLDgadFx5jE6EgvMoYfWyAsD4Q+eWOUsJeKPQVJ+pq1+PAfD2CIvqATQ9cbwdB04t89/1O/w1cDnyilFU='; //sesuaikan 
-$channelSecret = '6a0d4e83e1d0198373f3de1d03866d06';//sesuaikan
+$channelAccessToken = 'YOUR-CHANNEL-ACCESS-TOKEN'; //sesuaikan 
+$channelSecret = 'YOUR-CHANNEL-SECRET-CODE';//sesuaikan
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 $userId     = $client->parseEvents()[0]['source']['userId'];
 $groupId    = $client->parseEvents()[0]['source']['groupId'];
@@ -27,11 +22,11 @@ if (count($pesan_datang) > 2) {
     }
 }
 #-------------------------[Function]-------------------------#
-function sholat($keyword) {
+function shalat($keyword) {
     $uri = "https://time.siswadi.com/pray/" . $keyword;
     $response = Unirest\Request::get("$uri");
     $json = json_decode($response->raw_body, true);
-    $result = "Jadwal Sholat Sekitar ";
+    $result = "Jadwal Shalat Sekitar ";
     $result .= $json['location']['address'];
     $result .= "\nTanggal : ";
     $result .= $json['time']['date'];
@@ -55,7 +50,7 @@ function sholat($keyword) {
 # require_once('./src/function/hard.php');
 //show menu, saat join dan command /menu
 if ($type == 'join' || $command == '/menu') {
-    $text = "Assalamualaikum Kakak, aku adalah bot jadwal sholat, silahkan ketik\n\n/sholat <nama tempat>\n\nnanti aku bakalan kasih tahu jam berapa waktunya sholat ^_^";
+    $text = "Assalamualaikum Kakak, aku adalah bot jadwal shalat, silahkan ketik\n\n/shalat <nama tempat>\n\nnanti aku bakalan kasih tahu jam berapa waktunya shalat ^_^";
     $balas = array(
         'replyToken' => $replyToken,
         'messages' => array(
@@ -68,8 +63,62 @@ if ($type == 'join' || $command == '/menu') {
 }
 //pesan bergambar
 if($message['type']=='text') {
-        if ($command == '/sholat') {
-        $result = sholat($options);
+        if ($command == '/shalat') {
+        $result = shalat($options);
+        $balas = array(
+            'replyToken' => $replyToken,
+            'messages' => array(
+                array(
+                    'type' => 'text',
+                    'text' => $result
+                )
+            )
+        );
+    }
+}
+if (isset($balas)) {
+    $result = json_encode($balas);
+//$result = ob_get_clean();
+    file_put_contents('./balasan.json', $result);
+    $client->replyMessage($balas);
+}
+#-------------------------[Function]-------------------------#
+function cuaca($keyword) {
+    $uri = "http://api.openweathermap.org/data/2.5/weather?q=" . $keyword . ",ID&units=metric&appid=e172c2f3a3c620591582ab5242e0e6c4";
+    $response = Unirest\Request::get("$uri");
+    $json = json_decode($response->raw_body, true);
+    $result = "Halo Kak ^_^ Ini ada Ramalan Cuaca Untuk Daerah ";
+    $result .= $json['name'];
+    $result .= " Dan Sekitarnya";
+    $result .= "\n\nCuaca : ";
+    $result .= $json['weather']['0']['main'];
+    $result .= "\nDeskripsi : ";
+    $result .= $json['weather']['0']['description'];
+    return $result;
+}
+#-------------------------[Function]-------------------------#
+# require_once('./src/function/search-1.php');
+# require_once('./src/function/download.php');
+# require_once('./src/function/random.php');
+# require_once('./src/function/search-2.php');
+# require_once('./src/function/hard.php');
+//show menu, saat join dan command /menu
+if ($type == 'join' || $command == '/menu') {
+    $text = "Halo Kak ^_^\nAku Bot Prediksi Cuaca, Kamu bisa mengetahui prediksi cuaca di daerah kamu sesuai dengan sumber BMKG";
+    $balas = array(
+        'replyToken' => $replyToken,
+        'messages' => array(
+            array(
+                'type' => 'text',
+                'text' => $text
+            )
+        )
+    );
+}
+//pesan bergambar
+if($message['type']=='text') {
+        if ($command == '/cuaca') {
+        $result = cuaca($options);
         $balas = array(
             'replyToken' => $replyToken,
             'messages' => array(
